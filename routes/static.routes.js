0810a628098fb396c8b27792749be914ea4257c1
satyam123ee <satyam23164057@akgec.ac.in>
2026-08-10 02:52:13 +0530

    feat: enhance home page to display list of shortened URLs with details

--- routes/static.routes.js
+++ routes/static.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
+import urlModel from "../models/url.models.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+   const allUrls = await urlModel.find({});
     res.render("home", {
         shortId: null,
-        redirectURL: null
+        redirectURL: null,
+        urls: allUrls
+
     });
 });
 
